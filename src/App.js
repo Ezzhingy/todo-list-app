135091611f43393e7a0c0c610ebf083422b21294
Eugene Zhang <zhang.eug@gmail.com>
2022-12-29 12:32:49 -0500

    Make bubble width responsive

--- src/App.js
+++ src/App.js
@@ -1,64 +1,59 @@
-import { Sidebar } from './components/sidebar/Sidebar';
-import { Main } from './components/main/Main';
-import { ProjectModal, TaskModal } from "./components/project-modal/ProjectModal";
-import { useModalProject, useModalTask } from './hooks/useModal';
-import { allProjects } from './functions/factory';
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { Main } from "./components/main/Main";
+import {
+  ProjectModal,
+  TaskModal,
+} from "./components/project-modal/ProjectModal";
+import { useModalProject, useModalTask } from "./hooks/useModal";
+import { allProjects } from "./functions/factory";
 
-import './app.css';
+import "./app.css";
 
-import { useRef, useState, useEffect} from 'react';
+import { useRef, useState, useEffect } from "react";
 
 const App = () => {
-
   const [foo, setFoo] = useState(0);
 
   const titleRef = useRef(null);
-  const {isShowingProject, toggleProject} = useModalProject();
-  const {isShowingTask, toggleTask} = useModalTask();
+  const { isShowingProject, toggleProject } = useModalProject();
+  const { isShowingTask, toggleTask } = useModalTask();
 
   useEffect(() => {
-    const getProjectsString = localStorage.getItem('projects');
+    const getProjectsString = localStorage.getItem("projects");
     const getProjects = JSON.parse(getProjectsString);
-  
+
     if (getProjects !== null) {
-       
       for (let task in getProjects) {
         if (getProjects[task].taskArray.length !== 0) {
-
           let tempDate = getProjects[task].taskArray[0].date;
-          tempDate = tempDate.slice(0, 10).split('-');
-          if (tempDate[1] === '1') {
-              tempDate[1] = '12';
+          tempDate = tempDate.slice(0, 10).split("-");
+          if (tempDate[1] === "1") {
+            tempDate[1] = "12";
           } else {
-              tempDate[1] -= 1;
+            tempDate[1] -= 1;
           }
           const date = new Date(tempDate[0], tempDate[1], tempDate[2]);
           getProjects[task].taskArray[0].date = date;
-        
         }
       }
       allProjects.projectObj = getProjects;
       setFoo(foo + 1);
     }
   }, []);
-  
+
   return (
     <div className="App">
-      <div className='body-grid'>
-        <div className='sidebar-container'>
+      <div className="body-grid">
+        <div className="sidebar-container">
           <h1>projects</h1>
-          <Sidebar 
-            foo={foo}
-            setFoo={setFoo}
-          />
-          <button className="add-project-btn" onClick={toggleProject}>+ add new project</button>
-          <ProjectModal
-            isShowing={isShowingProject}
-            hide={toggleProject}
-          />
+          <Sidebar foo={foo} setFoo={setFoo} />
+          <button className="add-project-btn" onClick={toggleProject}>
+            + add new project
+          </button>
+          <ProjectModal isShowing={isShowingProject} hide={toggleProject} />
         </div>
-  
-        <div className='main-container'>
+
+        <div className="main-container">
           <ul className="bg-bubbles">
             <li></li>
             <li></li>
@@ -69,22 +64,24 @@ const App = () => {
             <li></li>
             <li></li>
             <li></li>
-            <li></li>
           </ul>
-          <h1 ref={titleRef} className='title'>todo list</h1>
-          <Main titleRef={titleRef}/>
-          <button className='add-task-btn' onClick={toggleTask}>+ add a task</button>
-          <TaskModal
-            isShowing={isShowingTask}
-            hide={toggleTask}
-          />
+          <h1 ref={titleRef} className="title">
+            todo list
+          </h1>
+          <Main titleRef={titleRef} />
+          <button className="add-task-btn" onClick={toggleTask}>
+            + add a task
+          </button>
+          <TaskModal isShowing={isShowingTask} hide={toggleTask} />
         </div>
       </div>
       <div className="footer">
-            <p>Copyright © <a href="https://github.com/Ezzhingy">Ezzhingy</a> 2022</p>
-      </div>  
+        <p>
+          Copyright © <a href="https://github.com/Ezzhingy">Ezzhingy</a> 2022
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+import { getFirebaseConfig } from "./components/firebase/firebase-config";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+const firebaseAppConfig = getFirebaseConfig();
+const app = initializeApp(firebaseAppConfig);
